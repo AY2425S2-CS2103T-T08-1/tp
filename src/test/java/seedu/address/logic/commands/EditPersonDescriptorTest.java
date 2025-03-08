b4394c389e8329910c8fdbb9f2d3f0855aa4f6aa
Clifong <59185915+Clifong@users.noreply.github.com>
2025-03-08 22:05:23 +0800

    Fix more tests issue
    The errors are due to checkstyle. I have fixed more of them
    after using the configuration file and checkstyle method thing.

--- src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
+++ src/test/java/seedu/address/logic/commands/EditPersonDescriptorTest.java
@@ -1,12 +1,9 @@
 package seedu.address.logic.commands;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_AMY;
 import static seedu.address.logic.commands.CommandTestUtil.DESC_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
-import static seedu.address.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
@@ -48,13 +45,12 @@ public class EditPersonDescriptorTest {
         editedAmy = new EditPersonDescriptorBuilder(DESC_AMY).withTags(VALID_TAG_HUSBAND).build();
         assertFalse(DESC_AMY.equals(editedAmy));
     }
-
-//    @Test
-//    public void toStringMethod() {
-//        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
-//        String expected = EditPersonDescriptor.class.getCanonicalName() + "{name="
-//                + editPersonDescriptor.getName().orElse(null) + ", phone="
-//                + editPersonDescriptor.getTags().orElse(null) + "}";
-//        assertEquals(expected, editPersonDescriptor.toString());
-//    }
+    //    @Test
+    //    public void toStringMethod() {
+    //        EditPersonDescriptor editPersonDescriptor = new EditPersonDescriptor();
+    //        String expected = EditPersonDescriptor.class.getCanonicalName() + "{name="
+    //                + editPersonDescriptor.getName().orElse(null) + ", phone="
+    //                + editPersonDescriptor.getTags().orElse(null) + "}";
+    //        assertEquals(expected, editPersonDescriptor.toString());
+    //}
 }
