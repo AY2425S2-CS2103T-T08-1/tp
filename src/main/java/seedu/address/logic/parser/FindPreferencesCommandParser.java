7426ac9e1797123af53e92a57ad167fcc303a124
Stanley Beh <ziliangstanley@gmail.com>
2025-03-20 00:59:57 +0800

    Debug and add tests for findPreferences

--- src/main/java/seedu/address/logic/parser/FindPreferencesCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindPreferencesCommandParser.java
@@ -1,25 +1,31 @@
 package seedu.address.logic.parser;
 
-import java.util.function.Predicate;
+import static java.util.Objects.requireNonNull;
 
 import seedu.address.logic.commands.FindPreferencesCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.PreferenceContainsKeywordPredicate;
 
 /**
  * Parses input arguments and creates a new FindPreferencesCommand object
  */
 public class FindPreferencesCommandParser implements Parser<FindPreferencesCommand> {
-    @Override
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the FindPreferencesCommand
+     * and returns a FindPreferencesCommand object for execution.
+     */
     public FindPreferencesCommand parse(String args) throws ParseException {
+        requireNonNull(args);
+
         String trimmedArgs = args.trim();
         if (trimmedArgs.isEmpty()) {
             throw new ParseException(String.format("Invalid command format! \n%s",
                     FindPreferencesCommand.MESSAGE_USAGE));
         }
 
-        Predicate<Person> predicate = new PreferenceContainsKeywordPredicate(trimmedArgs);
+        PreferenceContainsKeywordPredicate predicate = new PreferenceContainsKeywordPredicate(trimmedArgs);
         return new FindPreferencesCommand(predicate);
+
     }
 }
